8bf10b2f44bd43d5d1f79d89fe4edc0de58e84bb
rtang09 <49603415+rtang09@users.noreply.github.com>
2020-10-20 09:31:36 -0700

    Update numbermath.py

--- numbermath.py
+++ numbermath.py
@@ -2,9 +2,12 @@ def sqrt(x):
     return x**(1/2)
 def floor(x):
     return int(x)
-def product(l):
+def product(lst):
+    '''
+    returns the product of all the elements in a list.
+    '''
     p = 1
-    for x in l:
+    for x in lst:
         p = p*x
     return p
 class factor:
@@ -23,42 +26,54 @@ class factor:
                 f.append(i)
         return f
 class prime:
-    primes = []
-    def check(number):
-        number = int(number)
-        if number <= 1:
-            return "Not prime or composite"
-        if number == 2:
-            return 1
-        if number%2 == 0:
-            return 2
-        for i in range(3, floor(sqrt(number))+1, 2):
-            if number%i == 0:
-                return i
+    from math import *
+primes = []
+def check(number):
+    """
+    Checks if a number is prime
+    """
+    number = int(number)
+    if number <= 1:
+        return "Not prime or composite"
+    if number == 2:
         return 1
-
-    def listprime(start=1, stop=100):
-        primes = []
-        for i in range(start, stop+1):
-            number = prime.check(i)
-            if number == 1:
-                primes.append(i)
-        return primes
-    def num_primes(start=1, stop=100):
-        primes = []
-        for i in range(start, stop+1):
-            number = prime.check(i)
-            if number == 1:
-                primes.append(i)
-        return len(primes)
-        
-    def number(number):
-        x = number+1
-        primes = prime.listprime(stop = x)
-        while len(primes) < number:
-            x += 1
-            primes = prime.listprime(stop = x)
-        return primes[number-1]
+    if number%2 == 0:
+        return 2
+    for i in range(3, floor(sqrt(number))+1, 2):
+        if number%i == 0:
+            return i
+    return 1
+def listprime(start=1, stop=100):
+    """
+    Lists primes between two numbers.
+    """
+    primes = []
+    for i in range(start, stop+1):
+        number = check(i)
+        if number == 1:
+            primes.append(i)
+    return primes
+def num_primes(start=1, stop=100):
+    """
+    Say how many primes are between two numbers
+    """
+    primes = []
+    for i in range(start, stop+1):
+        number = check(i)
+        if number == 1:
+            primes.append(i)
+    return len(primes)
+    
+def number(number):
+    """
+    Says the xth prime
+    """
+    x = number+1
+    primes = listprime(stop = x)
+    while len(primes) < number:
+        x += 1
+        primes = listprime(stop = x)
+    return primes[number-1]
 if __name__ == "__main__":
     print(factor.LCM(3,4))
     print(factor.GCD(3,4))
