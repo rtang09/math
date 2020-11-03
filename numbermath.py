fd18f19668eb0e4d0fb7779e2502b96b459e26b4
rtang09 <49603415+rtang09@users.noreply.github.com>
2020-11-03 11:19:59 -0800

    Update numbermath.py

--- numbermath.py
+++ numbermath.py
@@ -3,16 +3,19 @@ def piestimate(acuracy, digits):
     for num in range(1, acuracy, 4):
         start += 1/num - 1/(num+2)
     start = start*4
-    code = '''return "{:.{}f}".format(start)'''.format(digits)
-    return eval(code)
+    return float(str(round(start, digits+1))[:digits+2])
 def estimate_e(acuricy, digits):
     answer = ((acuricy+1)/acuricy)**acuricy
-    code = '''return "{:.{}f}".format(answer)'''.format(digits)
-    return eval(code)
+    return float(str(round(answer, digits+1))[:digits+2])
 def sqrt(x):
     return x**(1/2)
 def floor(x):
     return int(x)
+def factorial(x):
+    y = 1
+    for i in range(1, x+1):
+        y = y*i
+    return y
 def product(lst):
     '''
     returns the product of all the elements in a list.
@@ -21,66 +24,64 @@ def product(lst):
     for x in lst:
         p = p*x
     return p
-class factor:
-    def LCM(a, b):
-        for i in range(max([a,b]), a*b+1):
-            if i%a == 0 and i%b == 0:
-                return i
-    def GCD(a, b):
-        for i in range(1, a*b+1):
-            if a%i == 0 and b%1 == 0:
-                return i
-    def factorize(a):
-        f = []
-        for i in range(1, a+1):
+
+def LCM(a, b):
+    for i in range(max([a,b]), a*b+1):
+        if i%a == 0 and i%b == 0:
+            return i
+def GCD(a, b):
+    for i in range(1, a*b+1):
+        if a%i == 0 and b%1 == 0:
+            return i
+def factorize(a):
+    f = []
+    for i in range(1, a+1):
             if a%i == 0:
                 f.append(i)
-        return f
-class prime:
-    primes = []
-    def check(number):
-        """
-        Checks if a number is prime
-        """
-        number = int(number)
-        if number <= 1:
-            return "Not prime or composite"
-        if number == 2:
-            return 1
-        if number%2 == 0:
-            return 2
-        for i in range(3, floor(sqrt(number))+1, 2):
-            if number%i == 0:
-                return i
+    return f
+primes = []
+def check_prime(number):
+    """
+    Checks if a number is prime
+    """
+    number = int(number)
+    if number <= 1:
+        return "Not prime or composite"
+    if number == 2:
         return 1
-    def listprime(start=1, stop=100):
-        """
-        Lists primes between two numbers.
-        """
-        primes = []
-        for i in range(start, stop+1):
-            number = check(i)
-            if number == 1:
-                primes.append(i)
-        return primes
-    def num_primes(start=1, stop=100):
-        """
-        Say how many primes are between two numbers
-        """
-        primes = []
-        for i in range(start, stop+1):
-            number = check(i)
-            if number == 1:
-                primes.append(i)
-        return len(primes)
-
-    def number(number):
-        """
-        Says the xth prime
-        """
-        x = number+1
+    if number%2 == 0:
+        return 2
+    for i in range(3, floor(sqrt(number))+1, 2):
+        if number%i == 0:
+            return i
+    return 1
+def listprime(start=1, stop=100):
+    """
+    Lists primes between two numbers.
+    """
+    primes = []
+    for i in range(start, stop+1):
+        number = check(i)
+        if number == 1:
+            primes.append(i)
+    return primes
+def num_primes(start=1, stop=100):
+    """
+    Say how many primes are between two numbers
+    """
+    primes = []
+    for i in range(start, stop+1):
+        number = check(i)
+        if number == 1:
+            primes.append(i)
+    return len(primes)
+def prime_number(number):
+    """
+    Says the xth prime
+    """
+    x = number+1
+    primes = listprime(stop = x)
+    while len(primes) < number:
+        x += 1
         primes = listprime(stop = x)
-        while len(primes) < number:
-            x += 1
-            primes = listprime(stop = x)
-        return primes[number-1]
+    return primes[number-1]
